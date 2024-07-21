4c3369e22a77bb8a8e5f90ebc672d53978be668a
cjburn3 <156855354+cjburn3@users.noreply.github.com>
2024-07-21 14:19:09 -0400

    updated page.js, layout.js, created management folder & utils folder

---
+++ src/app/management/page.js
@@ -0,0 +1,153 @@
+"use client"
+
+import React, { useState, useEffect } from 'react';
+import { db } from 'firebase.config';
+import { getAllDocuments, addDocument, deleteDocument, updateDocument } from '../utils/firebaseUtils';
+
+export default function Management() {
+  const [inventory, setInventory] = useState([]);
+  const [newInventoryName, setNewInventoryName] = useState('');
+  const [newInventorySymbol, setNewInventorySymbol] = useState('');
+  const [newInventoryPrice, setNewInventoryPrice] = useState('');
+  const [editInventoryId, setEditInventoryId] = useState(null);
+  const [editInventoryName, setEditInventoryName] = useState('');
+  const [editInventorySymbol, setEditInventorySymbol] = useState('');
+  const [editInventoryPrice, setEditInventoryPrice] = useState('');
+
+  useEffect(() => {
+    fetchInventory();
+  }, []);
+
+  const fetchInventory = async () => {
+    const querySnapshot = await getAllDocuments(db , "Inventory");
+    setInventory(querySnapshot);
+  };
+
+  const handleAddInventory = async () => {
+    if (newInventoryName.trim() !== '' && newInventorySymbol.trim() !== '' && newInventoryPrice.trim() !== '') {
+      const newInventory = {
+        name: newInventoryName,
+        symbol: newInventorySymbol,
+        price: parseFloat(newInventoryPrice),
+      };
+
+      const ref = await addDocument(db, "Inventory", newInventory) ;
+
+      fetchInventory();
+      setNewInventoryName('');
+      setNewInventorySymbol('');
+      setNewInventoryPrice('');
+
+      console.log("Inserted Object");
+
+      console.log(ref);
+    }
+  };
+
+  const handleEditInventory = async () => {
+    if (editInventoryId !== null && editInventoryName.trim() !== '' && editInventorySymbol.trim() !== '' && editInventoryPrice.trim() !== '') {
+      
+      const dataInfo = {
+        name: editInventoryName,
+        symbol: editInventorySymbol,
+        price: parseFloat(editInventoryPrice)
+      }
+
+      await updateDocument(db, "inventory", editInventoryId, dataInfo);
+
+      fetchInventory();
+      setEditInventoryId(null);
+      setEditInventoryName('');
+      setEditInventorySymbol('');
+      setEditInventoryPrice('');
+    }
+  };
+
+  const handleDeleteInventory = async (id) => {
+    await deleteDocument(db, "inventory", id);
+    fetchInventory();
+  };
+
+  const handleSetEditInventory = (stock) => {
+    setEditInventoryId(inventory.id);
+    setEditInventoryName(inventory.name);
+    setEditInventorySymbol(inventory.symbol);
+    setEditInventoryPrice(inventory.price.toString());
+  };
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Manage Inventory</h1>
+      <div className="mb-4">
+        <input
+          type="text"
+          value={editInventoryName}
+          onChange={(e) => setEditInventoryName(e.target.value)}
+          placeholder="Enter inventory name"
+          className="border rounded-md p-2 mr-2"
+        />
+        <input
+          type="text"
+          value={editInventorySymbol}
+          onChange={(e) => setEditInventorySymbol(e.target.value)}
+          placeholder="Enter inventory symbol"
+          className="border rounded-md p-2 mr-2"
+        />
+        <input
+          type="number"
+          value={editInventoryPrice}
+          onChange={(e) => setEditInventoryPrice(e.target.value)}
+          placeholder="Enter inventory price"
+          className="border rounded-md p-2 mr-2"
+        />
+        <button onClick={handleEditInventory} className="bg-blue-500 text-white px-4 py-2 rounded-md">
+          Update Inventory
+        </button>
+      </div>
+      <div className="mb-4">
+        <input
+          type="text"
+          value={newInventoryName}
+          onChange={(e) => setNewInventoryName(e.target.value)}
+          placeholder="Enter new inventory name"
+          className="border rounded-md p-2 mr-2 color-black"
+        />
+        <input
+          type="text"
+          value={newInventorySymbol}
+          onChange={(e) => setNewInventorySymbol(e.target.value)}
+          placeholder="Enter new inventory symbol"
+          className="border rounded-md p-2 mr-2 COLO"
+        />
+        <input
+          type="number"
+          value={newInventoryPrice}
+          onChange={(e) => setNewInventoryPrice(e.target.value)}
+          placeholder="Enter new inventory price"
+          className="border rounded-md p-2 mr-2"
+        />
+        <button onClick={handleAddInventory} className="bg-green-500 text-white px-4 py-2 rounded-md">
+          Add Inventory
+        </button>
+      </div>
+      <ul className="space-y-2">
+        {inventory.map((inventory) => (
+          <li key={Inventory.id} className="flex justify-between items-center bg-blue-300 p-2 rounded-md">
+            <div>
+              <p className="font-semibold">{Inventory.name} ({inventory.symbol})</p>
+              <p>Price: ${inventory.price}</p>
+            </div>
+            <div>
+              <button onClick={() => handleSetEditInventory(inventory)} className="text-blue-500 mr-2">
+                Edit
+              </button>
+              <button onClick={() => handleDeleteInventory(inventory.id)} className="text-red-500">
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/app/utils/firebaseUtils.js
@@ -0,0 +1,52 @@
+import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc } from "firebase/firestore";
+import { db } from "firebase.config";
+
+async function addDocument(db, collectionName, data) { //Create
+  try {
+    const docRef = await addDoc(collection(db, collectionName), data);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+async function getAllDocuments(db, collectionName) { //Read
+  const query = await getDocs( collection(db, collectionName) )
+  const documents= [];
+
+  query.forEach( (doc)=>{
+    documents.push( { id: doc.id, ...doc.data() } )  
+  } );
+  return documents;
+}
+
+async function updateDocument(db, collectionName, id, data){ //Update 
+
+  const updateInventory = doc(db, collectionName, id);
+ 
+  if (collectionName){
+    await updateDoc(updateInventory,data )
+  }else {
+    console.log("No references of doc");
+  }
+
+}
+
+async function deleteDocument(db, collectionName, id){ // Delete
+
+  const deleteRef = doc(db, collectionName, id);
+
+  if (deleteRef){
+    await deleteDoc(deleteRef);
+  }else {
+    console.log("Element does not exists");
+  }
+
+}
+
+
+
+
+
+
+export { getAllDocuments, addDocument, updateDocument, deleteDocument };
