3bf35b5fbb8d9fdc6896e99e39ea6983dddd6876
cjburn3 <156855354+cjburn3@users.noreply.github.com>
2024-07-22 21:51:31 -0400

    updates

--- firebase.config.js
+++ firebase.config.js
@@ -19,7 +19,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebase.config);
+const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db =getFirestore(app)
 
