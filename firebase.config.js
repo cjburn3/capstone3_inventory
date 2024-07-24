2349f66e3cd175ea6039428022e4304cf37b9d0e
cjburn3 <156855354+cjburn3@users.noreply.github.com>
2024-07-24 19:02:07 -0400

    updates

--- firebase.config.js
+++ firebase.config.js
@@ -9,14 +9,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyARQ2s68yC5o1fgVjSG8QVb7-hlhjmLG7U",
-  authDomain: "garage-inventory-81659.firebaseapp.com",
-  projectId: "garage-inventory-81659",
-  storageBucket: "garage-inventory-81659.appspot.com",
-  messagingSenderId: "866383758758",
-  appId: "1:866383758758:web:e93b43b7d1ac59a9603b67",
-  measurementId: "G-X5FBYWBWK2"
-}
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+ };
+ 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
