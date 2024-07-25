eddea226b111f4a24326e0b15a61b300719e9b37
cjburn3 <156855354+cjburn3@users.noreply.github.com>
2024-07-24 20:52:16 -0400

    created components folder,& register folder and created AuthUtils file

---
+++ src/app/registerForm/page.js
@@ -0,0 +1,12 @@
+"use client"
+import RegisterForm from "../components/RegisterForm";
+export default function RegisterFormPage() {
+
+   return (
+    <div className="container mx-auto p-4">
+        
+ <RegisterForm/>   
+
+    </div>
+  );
+}
